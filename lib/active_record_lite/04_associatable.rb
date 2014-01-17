f66269ba40a3bdf96f0647a655cce68791770980
katherinewallace <kwalla89@gmail.com>
2014-01-17 14:55:03 -0500

    Adds Associatable module with ::belongs_to and ::has_many methods

--- lib/active_record_lite/04_associatable.rb
+++ lib/active_record_lite/04_associatable.rb
@@ -10,34 +10,61 @@ class AssocOptions
   )
 
   def model_class
-    # ...
+    self.class_name.constantize
   end
 
   def table_name
-    # ...
+    model_class.table_name
   end
 end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-    # ...
+    defaults = {
+      :foreign_key => "#{name}_id".to_sym,
+      :class_name => "#{name.to_s.camelcase}",
+      :primary_key => :id
+    }
+    options = defaults.merge(options)
+    @foreign_key = options[:foreign_key]
+    @class_name = options[:class_name]
+    @primary_key = options[:primary_key]
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    # ...
+    defaults = {
+      :foreign_key => "#{self_class_name.underscore}_id".to_sym,
+      :class_name => "#{name.singularize.camelcase}",
+      :primary_key => :id
+    }
+    options = defaults.merge(options)
+    @foreign_key = options[:foreign_key]
+    @class_name = options[:class_name]
+    @primary_key = options[:primary_key]
   end
 end
 
 module Associatable
   # Phase IVb
   def belongs_to(name, options = {})
-    # ...
+    options = BelongsToOptions.new(name, options)
+    define_method(name) do
+      foreign_key = self.send(options.foreign_key)
+      model_class = options.model_class
+      model_class.where(:id => foreign_key).first
+    end
   end
 
   def has_many(name, options = {})
-    # ...
+    options = HasManyOptions.new(name.to_s, self.name, options)
+    define_method(name) do
+      primary_key = self.send(options.primary_key)
+      foreign_key = options.foreign_key
+      model_class = options.model_class
+      model_class.where(foreign_key => primary_key)
+    end
   end
 
   def assoc_options
@@ -46,5 +73,5 @@ module Associatable
 end
 
 class SQLObject
-  # Mixin Associatable here...
+  extend Associatable
 end
