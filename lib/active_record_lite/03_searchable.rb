ebf44352169fc37fdde417c912867d9478987b6f
katherinewallace <kwalla89@gmail.com>
2014-01-17 13:13:10 -0500

    Adds Searchable module to allow search by where

--- lib/active_record_lite/03_searchable.rb
+++ lib/active_record_lite/03_searchable.rb
@@ -2,11 +2,19 @@ require_relative 'db_connection'
 require_relative '02_sql_object'
 
 module Searchable
+
   def where(params)
-    # ...
+    where_line = params.keys.map { |key| "#{key} = ?"}.join(" AND ")
+    sql = <<-SQL
+    SELECT *
+    FROM #{self.table_name}
+    WHERE #{where_line}
+    SQL
+    results = DBConnection.execute(sql, *params.values)
+    self.parse_all(results)
   end
 end
 
 class SQLObject
-  # Mixin Searchable here...
+  extend Searchable
 end
