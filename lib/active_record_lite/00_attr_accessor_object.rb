794c9396cf304317578953df43e6a5dcb5c729cb
katherinewallace <kwalla89@gmail.com>
2014-01-17 09:51:54 -0500

    Adds an attribute accessor

--- lib/active_record_lite/00_attr_accessor_object.rb
+++ lib/active_record_lite/00_attr_accessor_object.rb
@@ -1,5 +1,10 @@
 class AttrAccessorObject
   def self.my_attr_accessor(*names)
-    # ...
+    names.each do |name|
+      define_method(name) { self.instance_variable_get("@#{name}".to_sym) }
+      define_method("#{name}=".to_sym) do |arg|
+        self.instance_variable_set("@#{name}".to_sym, arg)
+      end
+    end
   end
 end
